324f9e71228c91551c9f91a5736a33e98e67a448
bass9030 <bass90301.naver.comm@gmail.com>
2024-10-23 00:41:20 +0900

    상단 마진 추가

--- inject.js
+++ inject.js
@@ -1,16 +1,18 @@
 console.log("I'm Injected!");
 
-window.addEventListener('load', () => {
-    console.log('page loaded');
+window.addEventListener("load", () => {
+    console.log("page loaded");
     checkLoad();
-})
+});
 
 let ExamInfo;
 let ExamQuestions;
 
 async function checkLoad() {
-    let buttons = document.querySelectorAll('section > div:nth-child(1) > div > button');
-    if(!!!buttons.length) {
+    let buttons = document.querySelectorAll(
+        "section > div:nth-child(1) > div > button"
+    );
+    if (!!!buttons.length) {
         setTimeout(checkLoad, 1000);
         return;
     }
@@ -20,35 +22,40 @@ async function checkLoad() {
     ExamQuestions = await getExamQuestions(ExamInfo);
     console.log(ExamQuestions);
 
-    buttons.forEach(button => {
-        button.addEventListener('click', async (event) => {
-            if(!!!ExamInfo) return;
+    buttons.forEach((button) => {
+        button.addEventListener("click", async (event) => {
+            if (!!!ExamInfo) return;
             let questionNum = parseInt(event.target.children[0].innerText);
             let questionId = ExamQuestions[questionNum - 1].evalQuestionId;
             // console.log(questionId)
-            let solutionInfo = await parseSolutionInfo(await getSolution(questionId));
+            let solutionInfo = await parseSolutionInfo(
+                await getSolution(questionId)
+            );
             // console.log(solutionInfo);
-            let questionDiv = document.querySelector('section > div:nth-child(2) > div > div > div > div > div.swiper-no-swiping > div > div');
-            let detailsEl = document.querySelector('section > div:nth-child(2) > div > div > div > div > div.swiper-no-swiping > div > div > details');
-            
-            if(!!detailsEl) detailsEl.remove();
+            let questionDiv = document.querySelector(
+                "section > div:nth-child(2) > div > div > div > div > div.swiper-no-swiping > div > div"
+            );
+            let detailsEl = document.querySelector(
+                "section > div:nth-child(2) > div > div > div > div > div.swiper-no-swiping > div > div > details"
+            );
 
-            let details = document.createElement('details');
-            let summary = document.createElement('summary');
-            summary.textContent = '해설 보기';
+            if (!!detailsEl) detailsEl.remove();
 
-            let p = document.createElement('span');
-            p.className = 'solution-text';
+            let details = document.createElement("details");
+            let summary = document.createElement("summary");
+            summary.textContent = "해설 보기";
+
+            let p = document.createElement("span");
+            p.className = "solution-text";
             p.innerHTML = solutionInfo.expText;
 
             details.appendChild(summary);
             details.appendChild(p);
+            details.style.marginTop = "5em";
             questionDiv.appendChild(details);
-            window.MathJax.Hub.Typeset(p)
-        })
-    })
+            window.MathJax.Hub.Typeset(p);
+        });
+    });
 }
 
-function addMoreBtn() {
-
-}
+function addMoreBtn() {}
